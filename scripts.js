a248c075839ecab8283e9f71e364e4dcb2be7432
SauloMarcuz <136103022+SauloMarcuz@users.noreply.github.com>
2023-06-11 13:45:15 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,62 +1,3 @@
-const numberContainer = document.querySelector('.numbers-container');
-const participantList = document.getElementById('participant-list');
-
-const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
-let selectedNumbers = [];
-let participants = [];
-
-// Função para atualizar a lista de números selecionados
-function updateSelectedNumbers() {
-  numberContainer.innerHTML = '';
-
-  numbers.forEach(number => {
-    const numberElement = document.createElement('div');
-    numberElement.classList.add('number');
-    numberElement.textContent = number;
-
-    if (selectedNumbers.includes(number) || isNumberTaken(number)) {
-      numberElement.classList.add('selected');
-      numberElement.setAttribute('disabled', true);
-    }
-
-    numberElement.addEventListener('click', () => {
-      if (!numberElement.classList.contains('selected')) {
-        numberElement.classList.add('selected');
-        selectedNumbers.push(number);
-      } else {
-        numberElement.classList.remove('selected');
-        selectedNumbers = selectedNumbers.filter(n => n !== number);
-      }
-    });
-
-    numberContainer.appendChild(numberElement);
-  });
-}
-
-// Função para verificar se um número já foi selecionado por outro participante
-function isNumberTaken(number) {
-  for (const participant of participants) {
-    if (participant.numbers.includes(number)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// Função para atualizar a lista de participantes
-function updateParticipantList() {
-  participantList.innerHTML = '';
-
-  participants.forEach(participant => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-item');
-    listItem.textContent = `${participant.name} - Números: ${participant.numbers.join(', ')}`;
-
-    participantList.appendChild(listItem);
-  });
-}
-
-// Função para confirmar uma participação
 function submitRaffleForm() {
   const formInput = document.getElementById('name');
   const name = formInput.value.trim();
@@ -74,28 +15,42 @@ function submitRaffleForm() {
     updateSelectedNumbers();
     updateParticipantList();
 
-    // Salvar os participantes no arquivo answers.json usando a API do GitHub
-    const jsonData = JSON.stringify(participants);
-    const { Octokit } = require("@octokit/rest");
+    // Estruturação dos números
+    const structuredNumbers = Array.from({ length: 100 }, (_, i) => ({
+      number: i + 1,
+      taken: isNumberTaken(i + 1)
+    }));
 
-    const octokit = new Octokit({ auth: "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" });
-
-    octokit.repos.createOrUpdateFileContents({
-      owner: "SauloMarcuz",
-      repo: "rifa-isis",
-      path: "answers.json",
-      message: "Atualizar arquivo answers.json",
-      content: Buffer.from(jsonData).toString('base64'),
-      branch: "Saulo"
-    }).then(() => {
-      console.log("Dados salvos com sucesso!");
-    }).catch(error => {
-      console.error("Erro ao salvar os dados:", error);
+    const jsonData = JSON.stringify({
+      participants: participants,
+      numbers: structuredNumbers
     });
+
+    // Salvar os participantes no arquivo answers.json usando a API do GitHub
+    const githubApiUrl = "https://api.github.com/repos/SauloMarcuz/rifa-isis/contents/answers.json";
+    const token = "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";
+
+    fetch(githubApiUrl, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/vnd.github.v3+json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "Atualizar arquivo answers.json",
+        content: btoa(jsonData),
+        branch: "Saulo",
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Dados salvos com sucesso!");
+      })
+      .catch(error => {
+        console.error("Erro ao salvar os dados:", error);
+      });
   } else {
     alert('Por favor, preencha o nome e selecione pelo menos um número.');
   }
 }
-
-// Inicialização da página
-updateSelectedNumbers();
