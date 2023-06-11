2ea558ad1a87f33463d40808e5da8ff1edf1c2ec
SauloMarcuz <136103022+SauloMarcuz@users.noreply.github.com>
2023-06-11 13:20:36 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,101 +1,29 @@
-const formInput = document.getElementById('name');
-const numberContainer = document.querySelector('.numbers-container');
-const participantList = document.getElementById('participant-list');
-
-const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
-let selectedNumbers = [];
-let participants = [];
-
-// Função para atualizar a lista de números selecionados
-function updateSelectedNumbers() {
-  numberContainer.innerHTML = '';
-
-  numbers.forEach(number => {
-    const numberElement = document.createElement('div');
-    numberElement.classList.add('number');
-    numberElement.textContent = number;
-
-    if (selectedNumbers.includes(number) || isNumberTaken(number)) {
-      numberElement.classList.add('selected');
-      numberElement.setAttribute('disabled', true);
-    }
-
-    numberElement.addEventListener('click', () => {
-      if (!numberElement.classList.contains('selected')) {
-        numberElement.classList.add('selected');
-        selectedNumbers.push(number);
-      } else {
-        numberElement.classList.remove('selected');
-        selectedNumbers = selectedNumbers.filter(n => n !== number);
-      }
-    });
-
-    numberContainer.appendChild(numberElement);
-  });
-}
-
-// Função para verificar se um número já foi selecionado por outro participante
-function isNumberTaken(number) {
-  for (const participant of participants) {
-    if (participant.numbers.includes(number)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// Função para atualizar a lista de participantes
-function updateParticipantList() {
-  participantList.innerHTML = '';
-
-  participants.forEach(participant => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-item');
-    listItem.textContent = `${participant.name} - Números: ${participant.numbers.join(', ')}`;
-
-    participantList.appendChild(listItem);
-  });
-}
-
-// Função para confirmar uma participação
 function submitRaffleForm() {
-  const name = formInput.value.trim();
+  // Restante do código...
 
-  if (name && selectedNumbers.length > 0) {
-    const participant = {
-      name: name,
-      numbers: selectedNumbers
-    };
+  // Salvar os participantes no arquivo respostas.json usando a API do GitHub
+  const jsonData = JSON.stringify(participants);
+  const githubApiUrl = "https://api.github.com/repos/SauloMarcuz/rifa-isis/contents/respostas.json";
+  const token = "SEU_TOKEN_DE_ACESSO_PESSOAL";
 
-    participants.push(participant);
-    formInput.value = '';
-    selectedNumbers = [];
-
-    updateSelectedNumbers();
-    updateParticipantList();
-
-    // Salvar os participantes no arquivo respostas.json usando a API do GitHub
-    const jsonData = JSON.stringify(participants);
-    const { Octokit } = require("@octokit/rest");
-
-    const octokit = new Octokit({ auth: "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" });
-
-    octokit.repos.createOrUpdateFileContents({
-      owner: "SauloMarcuz",
-      repo: "rifa-isis",
-      path: "respostas.json",
+  fetch(githubApiUrl, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/vnd.github.v3+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
       message: "Atualizar arquivo respostas.json",
-      content: Buffer.from(jsonData).toString('base64'),
-      branch: "Saulo"
-    }).then(() => {
+      content: btoa(jsonData),
+      branch: "Saulo",
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
       console.log("Dados salvos com sucesso!");
-    }).catch(error => {
+    })
+    .catch(error => {
       console.error("Erro ao salvar os dados:", error);
     });
-  } else {
-    alert('Por favor, preencha o nome e selecione pelo menos um número.');
-  }
 }
-
-// Inicialização da página
-updateSelectedNumbers();
