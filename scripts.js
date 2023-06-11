16e0ac196c8f4b32aa0766c3c816416222e5c9f3
SauloMarcuz <136103022+SauloMarcuz@users.noreply.github.com>
2023-06-11 13:56:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,64 @@
+const formInput = document.getElementById('name');
+const numberContainer = document.querySelector('.numbers-container');
+const participantList = document.getElementById('participant-list');
+
+const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
+let selectedNumbers = [];
+let participants = [];
+
+// Função para atualizar a lista de números selecionados
+function updateSelectedNumbers() {
+  numberContainer.innerHTML = '';
+
+  numbers.forEach(number => {
+    const numberElement = document.createElement('div');
+    numberElement.classList.add('number');
+    numberElement.textContent = number;
+
+    if (selectedNumbers.includes(number) || isNumberTaken(number)) {
+      numberElement.classList.add('selected');
+      numberElement.setAttribute('disabled', true);
+    }
+
+    numberElement.addEventListener('click', () => {
+      if (!numberElement.classList.contains('selected')) {
+        numberElement.classList.add('selected');
+        selectedNumbers.push(number);
+      } else {
+        numberElement.classList.remove('selected');
+        selectedNumbers = selectedNumbers.filter(n => n !== number);
+      }
+    });
+
+    numberContainer.appendChild(numberElement);
+  });
+}
+
+// Função para verificar se um número já foi selecionado por outro participante
+function isNumberTaken(number) {
+  for (const participant of participants) {
+    if (participant.numbers.includes(number)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Função para atualizar a lista de participantes
+function updateParticipantList() {
+  participantList.innerHTML = '';
+
+  participants.forEach(participant => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+    listItem.textContent = `${participant.name} - Números: ${participant.numbers.join(', ')}`;
+
+    participantList.appendChild(listItem);
+  });
+}
+
+// Função para confirmar uma participação
 function submitRaffleForm() {
-  const formInput = document.getElementById('name');
   const name = formInput.value.trim();
 
   if (name && selectedNumbers.length > 0) {
@@ -15,18 +74,8 @@ function submitRaffleForm() {
     updateSelectedNumbers();
     updateParticipantList();
 
-    // Estruturação dos números
-    const structuredNumbers = Array.from({ length: 100 }, (_, i) => ({
-      number: i + 1,
-      taken: isNumberTaken(i + 1)
-    }));
-
-    const jsonData = JSON.stringify({
-      participants: participants,
-      numbers: structuredNumbers
-    });
-
     // Salvar os participantes no arquivo answers.json usando a API do GitHub
+    const jsonData = JSON.stringify(participants);
     const githubApiUrl = "https://api.github.com/repos/SauloMarcuz/rifa-isis/contents/answers.json";
     const token = "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";
 
@@ -55,30 +104,5 @@ function submitRaffleForm() {
   }
 }
 
-// Função para atualizar a lista de números selecionados
-function updateSelectedNumbers() {
-  numberContainer.innerHTML = '';
-
-  numbers.forEach(number => {
-    const numberElement = document.createElement('div');
-    numberElement.classList.add('number');
-    numberElement.textContent = number.number;
-
-    if (number.taken || selectedNumbers.includes(number.number)) {
-      numberElement.classList.add('selected');
-      numberElement.setAttribute('disabled', true);
-    }
-
-    numberElement.addEventListener('click', () => {
-      if (!numberElement.classList.contains('selected')) {
-        numberElement.classList.add('selected');
-        selectedNumbers.push(number.number);
-      } else {
-        numberElement.classList.remove('selected');
-        selectedNumbers = selectedNumbers.filter(n => n !== number.number);
-      }
-    });
-
-    numberContainer.appendChild(numberElement);
-  });
-}
+// Inicialização da página
+updateSelectedNumbers();
