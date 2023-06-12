09d4a86ad986818be583dd5d24122a44fab9d9de
SauloMarcuz <136103022+SauloMarcuz@users.noreply.github.com>
2023-06-12 00:21:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,97 +1,109 @@
-const formEntrada = document.getElementById('nome');
-const numberContainer = document.querySelector('.numbers-container');
-const participanteLista = document.getElementById('lista-participantes');
+const  formEntrada  =  documento . getElementById ( 'nome' ) ;
+const  numberContainer  =  documento . querySelector ( '.numbers-container' ) ;
+const  participanteLista  =  documento . getElementById ( 'lista de participantes' ) ;
 
-const numeros = Array.from({ comprimento: 100 }, (_, i) => i + 1);
-let numerosSelecionados = [];
-let participantes = [];
+ números  const =  Array . from ( {  comprimento : 100  } ,  ( _ ,  i )  =>  i  +  1 ) ;
+deixe  Números selecionados  =  [ ] ;
+deixe  os participantes  =  [ ] ;
 
 // Função para atualizar a lista de números selecionados
-function atualizarNumerosSelecionados() {
-  numberContainer.innerHTML = '';
+function  updateSelectedNumbers ( )  {
+  numberContainer . innerHTML  =  '' ;
 
-  numeros.forEach(numero => {
-    const numeroElemento = document.createElement('div');
-    numeroElemento.classList.add('numero');
-    numeroElemento.textContent = numero;
+  números . forCada ( número  =>  {
+    const  numeroElemento  =  documento . createElement ( 'div' ) ;
+    númeroElemento . classList . adicionar ( 'número' ) ;
+    númeroElemento . textoConteúdo  =  número ;
 
-    if (numerosSelecionados.includes(numero) || isNumeroUtilizado(numero)) {
-      numeroElemento.classList.add('selecionado');
-      numeroElemento.setAttribute('desabilitado', true);
+    if  ( números selecionados . inclui ( número )  ||  isNumberTaken ( número ) )  {
+      númeroElemento . classList . add ( 'selecionado' ) ;
+      númeroElemento . setAttribute ( 'desabilitado' ,  verdadeiro ) ;
     }
 
-    numeroElemento.addEventListener('click', () => {
-      if (!numeroElemento.classList.contains('selecionado')) {
-        numeroElemento.classList.add('selecionado');
-        numerosSelecionados.push(numero);
-      } else {
-        numeroElemento.classList.remove('selecionado');
-        numerosSelecionados = numerosSelecionados.filter(n => n !== numero);
+    númeroElemento . addEventListener ( 'clique' ,  ( )  =>  {
+      if  ( ! numberElement . classList . contém ( 'selecionado' ) )  {
+        númeroElemento . classList . add ( 'selecionado' ) ;
+        Números selecionados . empurre ( número ) ;
+      }  senão  {
+        númeroElemento . classList . remover ( 'selecionado' ) ;
+        números selecionados  =  números selecionados . filtro ( n  =>  n  !==  número ) ;
       }
-    });
+    } ) ;
 
-    numberContainer.appendChild(numeroElemento);
-  });
+    numberContainer . appendChild ( númeroElemento ) ;
+  } ) ;
 }
 
 // Função para verificar se um número já foi selecionado por outro participante
-function isNumeroUtilizado(numero) {
-  for (const participante of participantes) {
-    if (participante.numeros.includes(numero)) {
-      return true;
+função  éNumberTaken ( número )  {
+  for  ( const  participante  de  participantes )  {
+    if  ( participante . números . inclui ( número ) )  {
+      retorna  verdadeiro ;
     }
   }
-  return false;
+  retorna  falso ;
 }
 
 // Função para atualizar a lista de participantes
-function atualizarListaParticipantes() {
-  participanteLista.innerHTML = '';
+function  updateParticipantList ( )  {
+  lista de participantes . innerHTML  =  '' ;
 
-  participantes.forEach(participante => {
-    const itemLista = document.createElement('li');
-    itemLista.classList.add('item-lista');
-    itemLista.textContent = `${participante.nome} - Números: ${participante.numeros.join(', ')}`;
+  participantes . forCada ( participante  =>  {
+    const  itemlista  =  documento . criarElemento ( 'li' ) ;
+    listaItem . classList . add ( 'lista-item' ) ;
+    listaItem . textContent  =  ` ${ participante . nome } - Números: ${ participante . números . join ( ', ' ) } ` ;
 
-    participanteLista.appendChild(itemLista);
-  });
+    lista de participantes . appendChild ( listItem ) ;
+  } ) ;
 }
 
 // Função para confirmar uma participação
-function enviarFormularioRifa() {
-  const nome = formEntrada.value.trim();
+função  enviarRaffleForm ( )  {
+  const  nome  =  formInput . valor . aparar ( ) ;
 
-  if (nome && numerosSelecionados.length > 0) {
-    const participante = {
-      nome: nome,
-      numeros: numerosSelecionados
-    };
+  if  ( nome  &&  números selecionados . comprimento  >  0 )  {
+     participante  constante =  {
+      nome : nome ,
+      números : números selecionados
+    } ;
 
-    participantes.push(participante);
-    formEntrada.value = '';
-    numerosSelecionados = [];
+    participantes . empurrar ( participante ) ;
+    formInput . valor  =  '' ;
+    Números selecionados  =  [ ] ;
 
-    atualizarNumerosSelecionados();
-    atualizarListaParticipantes();
+    atualizarNúmerosSelecionados ( ) ;
+    atualizarParticipantList ( ) ;
 
-    // Salvar os participantes no arquivo answers.json usando a API do GitHub
-    const jsonData = JSON.stringify(participantes);
-    const githubApiUrl = 'https://api.github.com/repos/SauloMarcuz/rifa-isis/contents/answers.json';
-    const githubToken = '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';
+    // Salvar os participantes no arquivo answers.json using a API do GitHub
+    const  jsonData  =  JSON . stringify ( participantes ) ;
+    const  githubApiUrl  =  'https://api.github.com/repos/SauloMarcuz/rifa-isis/contents/answers.json' ;
+    const  githubToken  =  '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' ;
 
-    fetch(githubApiUrl, {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${githubToken}`,
-        Accept: 'application/vnd.github.v3+json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: 'Atualizar arquivo answers.json',
-        content: btoa(jsonData),
-        branch: 'principal',
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Dados salvos com sucesso
+    buscar ( githubApiUrl ,  {
+      método : 'PUT' ,
+      cabeçalhos : {
+        Autorização : `Bearer ${ githubToken } ` ,
+        Aceitar : 'application/vnd.github.v3+json' ,
+        'Tipo de conteúdo' : 'aplicativo/json' ,
+      } ,
+      corpo : JSON . stringify ( {
+        message : 'Atualizar arquivo answers.json' ,
+        conteúdo : btoa ( jsonData ) ,
+        ramificação : 'principal' ,
+      } ) ,
+    } )
+      . então ( resposta  =>  resposta . json ( ) )
+      . então ( dados  =>  {
+        console . log ( 'Dados salvos com sucesso!' ) ;
+      } )
+      . pegar ( erro  =>  {
+        console . error ( 'Erro ao salvar os dados:' ,  error ) ;
+      } ) ;
+  }  senão  {
+    alert ( 'Por favor, preencha o nome e selecione pelo menos um número.' ) ;
+  }
+}
+
+// Inicialização da página
+atualizarNúmerosSelecionados ( ) ;
+atualizarParticipantList ( ) ;
